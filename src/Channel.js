ede095490405aa5a6d774c4cbe191edcf318049f
solomon <solonjogogmail.com>
2024-06-30 14:48:26 +0300

    finished the main cards ui part

---
+++ src/Channel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Channel.css';
+
+function Channel() {
+    return (
+        <div className="channel">
+            <h1>Channel Page</h1>
+        </div>
+    );
+}
+
+
+export default Channel;
