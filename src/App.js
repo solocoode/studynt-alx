64995c0842f5058e63533615ab23f06fc2d49ff4
solomon <solonjogogmail.com>
2024-07-01 09:10:28 +0300

    channek card ui and chatbox ui done (design not finalized)

--- src/App.js
+++ src/App.js
@@ -1,174 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Home';
+import Channel from './Channel';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="appchild">
-        <Sidebar/>
-        <Cards/>
-        <Recents/>
+    <BrowserRouter>
+      <div className="App">
+      <Routes>
+            <Route index element={<Home/>}/> 
+            <Route path="/channel" element={<Channel />} /> 
+          </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
 
 
-function Navbar() {
-  return (
-     <nav className="navbar">
-
-        <div className="navbar-left">
-          <h1><a href="/"> STUDYNT </a></h1>
-        </div>
-
-        <div className="navbar-center">
-          <input
-            type="search"
-            className="search-bar rounded"
-            placeholder="Search..."
-          />
-        </div>
-
-        <div className="navbar-right">
-        <h3> <a href="/">Create</a> </h3>
-        </div>
-
-      </nav>
-  );
-}
-
-function Sidebar() {
-  return (
-    <div className="sidebar">
-      <Categories/>
-      <hr/>
-      <Channels/>
-    </div>
-  );
-}
-
-function Categories() {
-  return (
-    <div className="categories">
-      <div className="category-item">
-        <h3> <a href="/">Home</a> </h3>
-        <h3> <a href="/">Popular</a> </h3>
-        <h3> <a href="/">All</a> </h3>
-      </div>
-    </div>
-  );
-}
-
-function Channels() {
-  return (
-    <div className="channels">
-      <div className="channel">
-        <h4>COMMUNITIES</h4>
-        <h3> <a href="/">Math</a> </h3>
-        <h3> <a href="/">English</a> </h3>
-        <h3> <a href="/">Computer</a> </h3>
-        <h3> <a href="/">Math</a> </h3>
-        <h3> <a href="/">English</a> </h3>
-        <h3> <a href="/">Computer</a> </h3>
-      </div>
-    </div>
-  );
-}
-
-function Cards() {
-  return (
-    <div className="cards">
-      <div className="card">
-        <h5>s/Channel</h5>
-        <h6>How has addiction affected your life?</h6>
-        <p>Addiction can be a dark cloud that casts a long shadow.
-           It can seep into every corner of your life, affecting your health,
-            relationships, and even your sense of self. 
-             Finances can become strained as you prioritize your addiction, 
-             leading to debt or neglecting responsibilities.
-               The people you love may grow distant as trust erodes and broken promises pile up.
-                 The worst part can be the internal struggle – the guilt, shame, 
-                 and the constant battle between wanting to stop and the overwhelming 
-                 pull of the addictive behavior. 
-          Addiction can steal your joy and leave you feeling empty and alone. 
-        </p>
-        <hr/>
-
-        <h5>s/Channel</h5>
-        <h6>How has addiction affected your life?</h6>
-        <p>Addiction can be a dark cloud that casts a long shadow.
-           It can seep into every corner of your life, affecting your health,
-            relationships, and even your sense of self. 
-             Finances can become strained as you prioritize your addiction, 
-             leading to debt or neglecting responsibilities.
-               The people you love may grow distant as trust erodes and broken promises pile up.
-                 The worst part can be the internal struggle – the guilt, shame, 
-                 and the constant battle between wanting to stop and the overwhelming 
-                 pull of the addictive behavior. 
-          Addiction can steal your joy and leave you feeling empty and alone. 
-        </p>
-        <hr/>
-      </div>
-    </div>
-  );
-}
-
-function Recents() {
-  return (
-    <div className="recents">
-      <div className="recent">
-        <h6>RECENT POSTS</h6>
-        <hr/>
-        <RecentsCard/>
-      </div>
-    </div>
-  );
-}
-
-function RecentsCard() {
-  return (
-    <div className="recentscard">
-      <div className="recentcard">
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-        
-        <h5>s/Channel</h5>
-        <p>Realm-based Hackathon - Aim for Impact</p>
-        <hr/>
-      </div>
-    </div>
-  );
-}
--- src/Channel.js
+++ src/Channel.js
@@ -4,10 +4,77 @@ import './Channel.css';
 function Channel() {
     return (
         <div className="channel">
-            <h1>Channel Page</h1>
+            <div className="channel-main">
+                <h1>s/Channel</h1>
+                <ChannelCard/>
+                <hr/>
+            </div>
+            <div className="channel-chat">
+                <ChatFunctionality/>
+            </div>
+        </div>
+    );
+}
+
+export default Channel;
+
+
+
+function ChannelCard() {
+    return (
+        <div className="channel-card">
+            <div className="channel-card-info">
+                <h6>u/UserName</h6>
+                <h4> * </h4>
+                <h6>23 hr, ago</h6>
+            </div>
+
+            <div className="channel-card-title">
+                <h3>Eductational Hackathon Study</h3>
+                <p>
+                We are a group of students from the SFU School of Interactive Arts and Technology, 
+                working on an exciting interface design project. 
+                Our aim is to gather insights from hackathon participants, 
+                mentors, and organizers regarding your experiences within the educational hackathon 
+                community. If you have ever mentored, participated in, or organized a hackathon before, 
+                we kindly ask you to take the time to fill out the appropriate survey below. 
+                Your participation will help contribute valuable input that can shape a platform tailored 
+                to meet the needs, challenges, and aspirations of educational hackathon stakeholders at 
+                all levels.
+                </p>
+            </div>
         </div>
     );
 }
 
 
-export default Channel;
+function ChatFunctionality() {
+    //const [comment, setComment] = useState(''); // State to store comment text
+  
+    //const handleChange = (event) => {
+      //setComment(event.target.value);
+    //};
+  
+    //const handleSubmit = (event) => {
+      //event.preventDefault(); // Prevent default form submission behavior
+      // Implement logic to handle comment submission (e.g., send to server)
+      //console.log('Submitted comment:', comment);
+      //setComment(''); // Clear comment after submission (optional)
+    //};
+    return (
+        <div className="chat-functionality">
+            <textarea
+                    className="chat-input"
+                    placeholder="Type message..."
+                    //value={comment}
+                    //onChange={handleChange}
+                />
+                <button type="submit" className="chat-send">
+                    Send
+                </button>
+        </div>
+    );
+}
+
+
+// add sidebar
