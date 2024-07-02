88b04029db4bedc978569bf5c3e9f948c3e7942d
solomon <solonjogogmail.com>
2024-07-02 17:30:04 +0300

    create post page ui preliminary complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Home';
 import Channel from './Channel';
+import Create from './Create';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
             <Route index element={<Home/>}/> 
             <Route path="/channel" element={<Channel />} /> 
+            <Route path="/create" element={<Create/>}/>
           </Routes>
       </div>
     </BrowserRouter>
